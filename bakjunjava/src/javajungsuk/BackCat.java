615100cf8fdc396925468d006d26bfcc27277ebf
admin <admin@DESKTOP-C673LUL>
2022-06-22 11:11:35 +0900

    cat

---
+++ bakjunjava/src/javajungsuk/BackCat.java
@@ -0,0 +1,10 @@
+package javajungsuk;
+
+public class BackCat {
+	public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
