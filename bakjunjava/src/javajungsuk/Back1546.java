1d11f9ec0967794fefa64d22299cb69cee814bf0
admin <haseohee.emma@gmail.com>
2022-06-24 12:20:17 +0900

    백준1546 2번쨰

--- bakjunjava/src/javajungsuk/Back1546.java
+++ bakjunjava/src/javajungsuk/Back1546.java
@@ -1,40 +1,37 @@
 package javajungsuk;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Back1546 {
 
-
-	    public static void main(String[] args){
-	        Scanner sc = new Scanner(System.in);
-	        int n = sc.nextInt();
-	        // 과목의 개수를 입력받는다.
-	        float[] subject = new float[n];
-	        // 입력받은 개수만큼 과목의 점수를 배열로 설정한다.
-	        float max = 0;
-	        // 최대값 변수 선언
-	        float avg = 0;
-	        // 평균값 변수 선언 
-	        
-	        for(int i=0; i <subject.length; i++){
-	            subject[i] = sc.nextInt();
-	            // 입력받은 수를 과목 배열에 반복문을 통해 삽입한다.
-	            if(subject[i] > max){
-	                //만약 입력받은 과목의 점수가 최댓값보다 크다면
-	                max = subject[i];
-	                // 그 값을 최댓값에 넣는다.
-	            }
-	        }
-	        
-	        for(int j=0; j<subject.length; j++){
-	            avg += (subject[j]/max *100)/n;
-	            // 반복문을 통해 
-	            // 입력받은 수들의 평균값을 구한다.
-	            // 입력받은 수 / 최댓값 + 100
-	            // 전부를 다시 입력받은 과목의 갯수로 나눠서 
-	            // 평균값을 구한다
-	        }
-	        System.out.println(avg);
-	    }
-
+	public static void main(String[] args) throws Exception{
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine()); 
+		//  과목의 갯수를 입력받기 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 과목의 점수들을 공백 기준으로 나눠서 받아야 하기에 StringTokenizer 클래스 생성 
+		int max = 0;
+		// 최댓값 변수 선언 
+		double sum =0;
+		// 과목의 합계 변수 선언 
+		for(int i =0; i<n; i++) {
+			int subject = Integer.parseInt(st.nextToken());
+			// 반복문으로 과목의 갯수만큼 과목의 점수를 입력받기 
+			
+			if(subject > max) {
+				max = subject;
+				// 입력받는 과목의 점수에서 최댓값이 나오면 max에 대입하기 
+			
+			}
+			
+			sum += subject;
+			
+		}
+		
+		System.out.println((sum/max) * 100 /n);
+ 	}
 }
